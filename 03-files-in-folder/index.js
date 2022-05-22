850968490d7e8027e3de9e8f6231ecb37e14a2c3
Sergei-Eremin <serezhanv@gmail.com>
2022-05-22 18:50:57 +0300

    feat: 4 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const fs = require("fs");
 
 fs.readdir(`${__dirname}/secret-folder`, { withFileTypes: true }, (err, files)=>{
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
-
+const fs = require('fs');
+fs.readdir(__dirname, (err) => {
+    if (err) throw err;
+  
+    // пути до изначальных папок и до скопированной папки
+    const pathToOriginalDir = `${__dirname}\\files`;
+    const pathToCopiedDir = `${__dirname}\\files-copy`;
+  
+    // создаю папку по пути dirname/название папки
+    fs.mkdir(pathToCopiedDir, (error) =>
+      console.log(`Путь до скопированной папки: ${pathToCopiedDir}`)
+    );
+  
+    fs.readdir(pathToOriginalDir, (err, files) => {
+      if (err) throw err;
+  
+      files.forEach((elem) => {
+        // путь до элементов
+        const pathToOldFile = pathToOriginalDir + '\\' + elem;
+        const pathToNewFile = pathToCopiedDir + '\\' + elem;
+  
+        // копирование
+        fs.copyFile(pathToOldFile, pathToNewFile, (error) =>
+          console.log(`Файл ${elem} успешно скопирован`)
+        );
+      });
+    });
+  });
